6b0b17a49bcb4674410620322c5ab45dcf30726e
Andrew Westgerdes <mintk@Andrews-MacBook-Pro.local>
2019-08-11 09:19:23 -0500

    updated css path

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import './assets/css/App.css';
 
 class App extends Component {
   render() {
